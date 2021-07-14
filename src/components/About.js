4db0eeac6a5b1638db7ff3bcc6d8fd0494cf9b3e
Joumana Laude <30224508+JoumanaLaude@users.noreply.github.com>
2021-07-14 19:30:08 -0400

    img widths defined

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,9 @@
 import React, { Component } from 'react';
-import { Row } from 'reactstrap';
+import { Col, Row } from 'reactstrap';
 import halohalo from '../assets/images/food/halo-halo.jpg';
 import coffeemug from '../assets/images/pages/yellowmug-coffee-book.png';
 import { AboutUsContent } from '../constants/aboutConst';
 import TitleText from './TitleText';
-
 
 class About extends Component {
    render() {
@@ -12,32 +11,29 @@ class About extends Component {
          <>
             <div className="container mt-5 pt-5 px-5">
                <Row>
-                  <div className="col col-sm pr-1">
+                  <Col lg={6} pr={1}>
                      <TitleText titleText={AboutUsContent.aboutUs.titleText} bodyText={AboutUsContent.aboutUs.bodyText} />
                      <TitleText bodyText={AboutUsContent.aboutUs.bodyText2} />
-                  </div>
-                  <div className="col d-none d-lg-block my-3">
-                     <img src={halohalo} className="img-fluid rounded float-right pt-5 d-none d-md-block" alt="Halo-halo dessert" />
-                  </div>
+                  </Col>
+                  <Col>
+                     <img src={halohalo} width="600" height="300" className="img-fluid rounded float-right pt-5 d-none d-md-block" alt="Halo-halo dessert" />
+                  </Col>
                </Row>
             </div>
 
             <hr />
             <div className="container pt-3 pb-3 px-5">
                <Row>
-                  <div className="col">
+                  <Col>
                      <TitleText titleText={AboutUsContent.eventSection.titleText} bodyText={AboutUsContent.eventSection.bodyText} />
-                        <img src={coffeemug} className="img-fluid rounded mx-auto d-block" alt="Coffee mug on book" />
+                     <img src={coffeemug} width="800" height="300" className="img-fluid rounded mx-auto d-block" alt="Coffee mug on book" />
                      <TitleText bodyText={AboutUsContent.eventSection.bodyText2} />
-                  </div>
+                  </Col>
                </Row>
             </div>
-
          </>
-
       );
    }
 }
-
 
 export default About;
